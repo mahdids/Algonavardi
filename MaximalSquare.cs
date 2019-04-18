4ba6d2b203efbb53d498396a97df912b7513cb26
mahdids <41722787+mahdids@users.noreply.github.com>
2019-04-18 19:48:47 +0430

    Update MaximalSquare.cs

--- MaximalSquare.cs
+++ MaximalSquare.cs
@@ -1,28 +1,31 @@
-  public class Solution
-    {
-        public static int MaximalSquare(char[][] matrix)
-        {
-
-            var max = 0;
+public class Solution {
+    public int MaximalSquare(char[][] matrix) {
+        if(matrix.Length==0 ||matrix[0].Length==0 )
+            return 0;
+        var max = 0;
             var x = matrix.Length;
-            var y = 0;
-            if (x != 0)
-                y = matrix[0].Length;
+            var y = matrix[0].Length;
             var result = new int[x + 1, y + 1];
 
             for (int i = 1; i < x + 1; i++)
+            {
                 for (int j = 1; j < y + 1; j++)
+                {
                     if (matrix[i - 1][j - 1] == '0')
+                    {
                         result[i, j] = 0;
+                    }
                     else
                     {
-                        result[i, j] = 1;
                         var min = Math.Min(Math.Min(result[i - 1, j], result[i, j - 1]), result[i - 1, j - 1]);
                         result[i, j] = min + 1;
-
                         if (result[i, j] > max)
+                        {
                             max = result[i, j];
+                        }
                     }
+                }
+            }
             return max * max;
-        }
     }
+}
